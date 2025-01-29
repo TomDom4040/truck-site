e03afa2bb205dcbf4515b4a588e2174065c820a6
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2025-01-29 09:30:28 +0700

    comp1

--- database/migrations/2025_01_26_035223_create_users_table.php
+++ database/migrations/2025_01_26_035223_create_users_table.php
@@ -1,8 +1,10 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -36,4 +38,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
--- database/migrations/2025_01_27_024223_add_phone_to_users_table.php
+++ database/migrations/2025_01_27_024223_add_phone_to_users_table.php
@@ -9,17 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->string('phone')->nullable(); // Добавление поля для телефона
-    });
-}
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
 
-public function down()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('phone');
-    });
-}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
 };
